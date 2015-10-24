9dbcb0a232a6000965b02e06b2e18c8c51509a57
Antonio Menarde <amenarde@gmail.com>
2015-10-24 15:38:16 -0400

    small decleration comment updates

--- 10-4-15 IMU Gyro Test/rotmatrix.m
+++ 10-4-15 IMU Gyro Test/rotmatrix.m
@@ -1,4 +1,10 @@
+% Rotation Matrix
+% Takes the components of a unit vector in 3d space (x y z) and three angle measures around those axes (tx ty tz)
+% Returns the vector formed when the inputted unit vector is rotated at the inputted angles
+% Author: Chris Powell
+
 function pos_ = rotmatrix(x, y, z, tx, ty, tz)
+ % Calculate the three of rotation matrices in 3d space
  rx=[1 0 0;
      0 cos(tx) -sin(tx);
      0 sin(tx) cos(tx)];
@@ -8,6 +14,8 @@ function pos_ = rotmatrix(x, y, z, tx, ty, tz)
  rz=[cos(tz) -sin(tz) 0;
      sin(tz) cos(tz) 0;
      0 0 1];
+ % Calculate total rotation matrix by multiplying together the x, y, and z rotation matrices
  r_=rx*ry*rz
- pos_=(r_*[x;y;z])';
+ % Calculate final vector by multiplying total rotation matrix by inputted vector
+ pos_=(r_*[x;y;z])'; % Return
 end
